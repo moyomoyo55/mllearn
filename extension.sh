a0ebb3f5e899451e2c4f383a6ace6ce04eb9d17c
moyomoyo55 <tak.kjm@gmail.com>
2022-09-04 14:07:52 +0000

    auto commit

--- extension.sh
+++ extension.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 code --install-extension ms-python.python
 code --install-extension albert.TabOut
+
+
+while true; do
+    git pull
+    git add .
+    git commit -m "auto commit"
+    git push
+    sleep 300
+done
